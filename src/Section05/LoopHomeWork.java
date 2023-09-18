99200a35c7fa3b67c690a38b302fdf38e24fd217
Mun <Mun@DESKTOP-2EBJSRM>
2023-09-18 21:26:18 +0900

    안올라가

--- src/Section05/LoopHomeWork.java
+++ src/Section05/LoopHomeWork.java
@@ -34,5 +34,19 @@ package Section05;
  */
 
 public class LoopHomeWork {
+	public static void main(String[] args) {
+	
+		
+		for(int i=0; i < 7; i++) {
+			for(int j = 0; j < i+1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+	}
 
 }
